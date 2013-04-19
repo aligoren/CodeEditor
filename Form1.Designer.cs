092515c765c553e6ad688c5b73bf3d135b4002c1
Ali GOREN <goren.ali@yandex.com>
2013-04-20 00:51:08 +0300

    CodeEditor

---
+++ Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.yapistirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kopyalaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rnkEd = new ICSharpCode.TextEditor.TextEditorControl();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.btnSec = new System.Windows.Forms.Button();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnAc = new System.Windows.Forms.Button();
+            this.dosyaAc = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // yapistirToolStripMenuItem
+            // 
+            this.yapistirToolStripMenuItem.Name = "yapistirToolStripMenuItem";
+            this.yapistirToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.yapistirToolStripMenuItem.Text = "Yapistir";
+            this.yapistirToolStripMenuItem.Click += new System.EventHandler(this.yapistirToolStripMenuItem_Click);
+            // 
+            // kopyalaToolStripMenuItem
+            // 
+            this.kopyalaToolStripMenuItem.Name = "kopyalaToolStripMenuItem";
+            this.kopyalaToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.kopyalaToolStripMenuItem.Text = "Kopyala";
+            this.kopyalaToolStripMenuItem.Click += new System.EventHandler(this.kopyalaToolStripMenuItem_Click);
+            // 
+            // rnkEd
+            // 
+            this.rnkEd.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rnkEd.ContextMenuStrip = this.contextMenuStrip1;
+            this.rnkEd.IsReadOnly = false;
+            this.rnkEd.Location = new System.Drawing.Point(22, 12);
+            this.rnkEd.Name = "rnkEd";
+            this.rnkEd.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.rnkEd.ShowVRuler = false;
+            this.rnkEd.Size = new System.Drawing.Size(801, 479);
+            this.rnkEd.TabIndex = 0;
+            this.rnkEd.Load += new System.EventHandler(this.rnkEd_Load);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton1.Location = new System.Drawing.Point(30, 503);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(55, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "C/C++";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton2.Location = new System.Drawing.Point(92, 503);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(38, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "C#";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // btnSec
+            // 
+            this.btnSec.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSec.Location = new System.Drawing.Point(517, 500);
+            this.btnSec.Name = "btnSec";
+            this.btnSec.Size = new System.Drawing.Size(75, 23);
+            this.btnSec.TabIndex = 3;
+            this.btnSec.Text = "Seç";
+            this.btnSec.UseVisualStyleBackColor = true;
+            this.btnSec.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton3.Location = new System.Drawing.Point(137, 503);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(46, 17);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "PHP";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton4.Location = new System.Drawing.Point(190, 503);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(74, 17);
+            this.radioButton4.TabIndex = 5;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "JavaScript";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton5.Location = new System.Drawing.Point(271, 503);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(45, 17);
+            this.radioButton5.TabIndex = 6;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "CSS";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton8.Location = new System.Drawing.Point(457, 503);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(54, 17);
+            this.radioButton8.TabIndex = 9;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "HTML";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton7.Location = new System.Drawing.Point(395, 503);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(46, 17);
+            this.radioButton7.TabIndex = 8;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "XML";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton6.Location = new System.Drawing.Point(333, 503);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(45, 17);
+            this.radioButton6.TabIndex = 7;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "SQL";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnKaydet.Location = new System.Drawing.Point(728, 500);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(75, 23);
+            this.btnKaydet.TabIndex = 10;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnAc
+            // 
+            this.btnAc.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAc.Location = new System.Drawing.Point(626, 500);
+            this.btnAc.Name = "btnAc";
+            this.btnAc.Size = new System.Drawing.Size(75, 23);
+            this.btnAc.TabIndex = 11;
+            this.btnAc.Text = "Aç";
+            this.btnAc.UseVisualStyleBackColor = true;
+            this.btnAc.Click += new System.EventHandler(this.btnAc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(832, 532);
+            this.Controls.Add(this.btnAc);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.radioButton8);
+            this.Controls.Add(this.radioButton7);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.btnSec);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.rnkEd);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Basit Text Editör";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem yapistirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kopyalaToolStripMenuItem;
+        private ICSharpCode.TextEditor.TextEditorControl rnkEd;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button btnSec;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnAc;
+        private System.Windows.Forms.OpenFileDialog dosyaAc;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ICSharpCode.TextEditor.Document;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void rnkEd_Load(object sender, EventArgs e)
+        {
+            
+            string dizin = Application.StartupPath;
+            FileSyntaxModeProvider renklendirici;
+            if (Directory.Exists(dizin))
+            {
+                renklendirici = new FileSyntaxModeProvider(dizin);
+                HighlightingManager.Manager.AddSyntaxModeFileProvider(renklendirici);
+                //rnkEd.SetHighlighting();
+                
+            }
+            
+            
+        }
+
+        private void kopyalaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(rnkEd.Text);
+        }
+
+        private void yapistirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rnkEd.Text = Clipboard.GetText();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                rnkEd.SetHighlighting(radioButton1.Text);
+            }
+            else if (radioButton2.Checked == true)
+            {
+                rnkEd.SetHighlighting(radioButton2.Text);
+            }
+            else if (radioButton3.Checked == true)
+            {
+                rnkEd.SetHighlighting(radioButton3.Text);
+            }
+            else if (radioButton4.Checked == true)
+            {
+                rnkEd.SetHighlighting(radioButton4.Text);
+            }
+            else if (radioButton5.Checked == true)
+            {
+                rnkEd.SetHighlighting(radioButton5.Text);
+            }
+            else if (radioButton8.Checked == true)
+            {
+                rnkEd.SetHighlighting(radioButton8.Text);
+            }
+            else if (radioButton7.Checked == true)
+            {
+                rnkEd.SetHighlighting(radioButton7.Text);
+            }
+            else if (radioButton6.Checked == true)
+            {
+                rnkEd.SetHighlighting(radioButton6.Text);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog kaydet = new SaveFileDialog();
+            kaydet.Title = "Dosyayı Kaydet...";
+            kaydet.Filter = "(*.cs)|*.cs|(*.php)|*.php|(*.cpp)|*.cpp|(*.c)|*.c|(*.h)|*.h|(*.hpp)|*.hpp|(*.html)|*.html|(*.sql)|*.sql|(*.js)|*.js|(*.css)|*.css|(*.xml)|*.xml|Tüm dosyalar(*.*)|*.*";
+            kaydet.FilterIndex = 2;
+            kaydet.InitialDirectory = (Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+            kaydet.ShowDialog();
+            rnkEd.SaveFile(kaydet.FileName);
+            
+            
+        }
+
+        private void btnAc_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dosyaAc.Title = "Dosya Seçiniz...";
+                dosyaAc.Filter = "(*.cs)|*.cs|(*.php)|*.php|(*.cpp)|*.cpp|(*.c)|*.c|(*.h)|*.h|(*.hpp)|*.hpp|(*.html)|*.html|(*.sql)|*.sql|(*.js)|*.js|(*.css)|*.css|(*.xml)|*.xml|Tüm dosyalar(*.*)|*.*";
+                dosyaAc.FilterIndex = 2;
+                dosyaAc.InitialDirectory = (Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+                dosyaAc.Multiselect = true;
+                dosyaAc.ShowDialog();
+                rnkEd.LoadFile(dosyaAc.FileName);
+                this.Text = dosyaAc.FileName.ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Dosya Secmediniz");
+            }
+        }
+
+    }
+}
